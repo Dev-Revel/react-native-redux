5eeae0fc368662d95d5bc250c51a3ddb0ca64ad3
umagol <umagol97@gmail.com>
2021-10-03 10:00:21 +0530

    init project

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * React Native App Redux Tutorial
+ * https://devrevel.com
+ * @author Devrevel
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+export default  App = () => {
+  return (
+    <View>
+      <Text> React Native - Redux Tutorial </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    
+});
